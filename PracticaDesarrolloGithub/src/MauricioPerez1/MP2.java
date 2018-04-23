a2a0a66a9af4c607fb89ba94104c26b72b9d68ec
MauRi <MauRi@10.5.4.245>
2018-04-23 10:19:29 -0500

    Mensaje de prueba MP123123

---
+++ PracticaDesarrolloGithub/src/MauricioPerez1/MP2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MauricioPerez1;
+
+import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
+
+/**
+ *
+ * @author MauRi
+ */
+public class MP2 {
+    public static void main(String[] args) {
+    
+        System.out.println("lunes 23 de abril pruba!");
+    }
+    
+    
+}
