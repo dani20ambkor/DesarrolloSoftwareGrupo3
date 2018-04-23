dab5ddb4ad4696c5b02b67b268593868db15b499
MauRi <MauRi@10.5.4.245>
2018-04-23 10:20:42 -0500

    hhgdhgd

--- PracticaDesarrolloGithub/src/MauricioPerez1/MP2.java
+++ PracticaDesarrolloGithub/src/MauricioPerez1/MP2.java
@@ -15,6 +15,7 @@ public class MP2 {
     public static void main(String[] args) {
     
         System.out.println("lunes 23 de abril pruba!");
+        System.out.println("jajaja");
     }
     
     
